acdd27de32fbbb57fe60ca440aa96474e199d4bf
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-06-02 00:11:37 +0530

    completed maxchars

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	return solution1(str);
+}
+
+solution1 = (str) => {
+	const temp = {};
+	let max = 0;
+	let maxChar = '';
+
+	for (let char of str) {
+		if (temp[char]) {
+			temp[char]++;
+		} else {
+			temp[char] = 1;
+		}
+	}
+
+	for (let char in temp) {
+		if (temp[char] > max) {
+			max = temp[char];
+			maxChar = char;
+		}
+	}
+
+	return maxChar;
+};
 
 module.exports = maxChar;
