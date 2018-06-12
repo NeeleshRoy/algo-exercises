cde8283ab228be68dff29c0a667b91d54672393d
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-06-12 11:19:26 +0530

    vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,31 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	return solution2(str);
+}
+
+function solution2(str) {
+	let count = 0;
+	const checker = 'aeiou';
+	const input = str.toLowerCase().split('');
+	for (let i of input) {
+		if (checker.includes(i)) {
+			++count;
+		}
+	}
+	return count;
+}
+
+function solution1(str) {
+	let count = 0;
+	const input = str.toLowerCase().split('');
+	for (let i of input) {
+		if (i === 'a' || i === 'e' || i === 'i' || i === 'o' || i === 'u') {
+			++count;
+		}
+	}
+	return count;
+}
 
 module.exports = vowels;
